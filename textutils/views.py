52a029ea96cb84eec7e1af8ece15ec4f9bf8a109
Rohini-Hirekhan <rohinihirekhan2020@gmail.com>
2020-09-02 12:10:41 +0530

    first commit

--- textutils/views.py
+++ textutils/views.py
@@ -1,6 +1,4 @@
 
-
-# Views.py
 # I have created this file - Harry
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -9,15 +7,16 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
 def analyze(request):
     #Get the text
-    djtext = request.GET.get('text', 'default')
+    djtext = request.POST.get('text', 'default')
 
     # Check checkbox values
-    removepunc = request.GET.get('removepunc', 'off')
-    fullcaps = request.GET.get('fullcaps', 'off')
-    newlineremover = request.GET.get('newlineremover', 'off')
-    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
 
     #Check which checkbox is on
     if removepunc == "on":
@@ -26,17 +25,9 @@ def analyze(request):
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
+
         params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
-        djtext=analyzed
-
-    if(extraspaceremover=="on"):
-        analyzed = ""
-        for index, char in enumerate(djtext):
-            if not(djtext[index] == " " and djtext[index+1]==" "):
-                analyzed = analyzed + char
-
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        djtext=analyzed
+        djtext = analyzed
 
     if(fullcaps=="on"):
         analyzed = ""
@@ -46,15 +37,24 @@ def analyze(request):
         params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
         djtext = analyzed
 
+    if(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
+
     if (newlineremover == "on"):
         analyzed = ""
         for char in djtext:
             if char != "\n" and char!="\r":
                 analyzed = analyzed + char
-           
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        
-    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on"):
-       return HttpResponse("Please select any operation and try again")
 
-    return render(request,'analyze.html',params)
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on"):
+        return HttpResponse("please select any operation and try again")
+
+    return render(request, 'analyze.html', params)
